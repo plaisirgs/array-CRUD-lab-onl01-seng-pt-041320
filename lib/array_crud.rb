d02db4e5e76346d6a13b6e33ebfc724ba0edf091
GINELLE PLAISIR <plaisirginelle@gmail.com>
2020-04-20 04:45:49 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,7 +23,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array.index_number.last
+ 
 end
 
 def retrieve_first_element_from_array(array)
@@ -31,5 +31,5 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array.last
 end
