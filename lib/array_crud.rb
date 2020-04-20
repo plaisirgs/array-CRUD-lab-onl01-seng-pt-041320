39be30cd23013c850305ba4e64d2a06785e894d7
GINELLE PLAISIR <plaisirginelle@gmail.com>
2020-04-20 05:03:11 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -10,9 +10,9 @@ def add_element_to_end_of_array(array, element)
   array << element
 end
 
-def add_element_to_start_of_array(array, element)
-  
-end
+  def add_element_to_start_of_array(array, element)
+    
+  end
 
 def remove_element_from_end_of_array(array)
   array.pop
